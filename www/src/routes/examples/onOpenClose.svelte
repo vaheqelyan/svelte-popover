68ebd906ea62462bed61232a2bd52b9600baed3b
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:42:40 +0400

    Add onOpen/onClose sample

---
+++ www/src/routes/examples/onOpenClose.svelte
@@ -0,0 +1,48 @@
+<style>
+  .content {
+    padding: 10px;
+    background: #fff;
+  }
+  .link {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .link a {
+    margin-left: 10px;
+  }
+</style>
+
+<svelte:head>
+  <title>Example — Basic</title>
+  <meta name="description" content="Svelte-grid — Example — Basic" />
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,basic,showcase" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>onOpen/onClose</h1>
+
+<p>
+  <a href="examples/hover">← Arrow</a>
+</p>
+
+<p>
+  <code>on:open</code>
+  is dispatched/called when the content is mounted,
+  <code>on:close</code>
+  is dispatched/called after the content is unmounted from the dom
+</p>
+
+<div class="link">
+  <a target="_blank" href={`${openInSource}/basic.svelte`}>View this example's source</a>
+  <a target="_blank" href="">Open in repl</a>
+</div>
+
+<Popover on:open={() => alert('open')} on:close={() => alert('close')}>
+  <button slot="target">Click</button>
+  <div slot="content" class="content">Hello, world!</div>
+</Popover>
+
+<script>
+  import { openInSource } from './github.js';
+  import Popover from 'svelte-popover';
+</script>
