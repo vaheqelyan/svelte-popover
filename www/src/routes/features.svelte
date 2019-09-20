8b47fc1c3c87c4cc370fde9349365d9732c6a941
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:42:55 +0400

    Create page

---
+++ www/src/routes/features.svelte
@@ -0,0 +1,35 @@
+<style>
+  .nosupport {
+    opacity: 0.3;
+  }
+  @media only screen and (max-width: 400px) {
+    ul {
+      padding-left: 25px;
+    }
+  }
+  @media only screen and (max-width: 242px) {
+    ul {
+      padding-left: 13px;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Svelte-popover — Features</title>
+  <!-- <meta name="description" content=""/> -->
+  <meta name="keywords" content="Svelte,svelte,svelte.js,sveltejs,features,popover,svelte-popover" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>Features</h1>
+
+<ul>
+  <li>100% Svlete (or vanillajs) - no jQuery, no 3rd party dependencies</li>
+  <li>Compatible with Sapper (SSR Svelte)</li>
+  <li>Auto placement (Work in progress)</li>
+  <li>Click, hover and touch events</li>
+  <li>With arrow and without</li>
+  <li class="nosupport">Animation/Transition</li>
+</ul>
+
+<a href="https://github.com/vaheqelyan/svelte-popover/issues/new" target="_blank">Suggest your feature</a>
