a4f7e9a850083c2ecc6b9a3383a7de57c9f5d4ce
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:42:29 +0400

    Create index page

---
+++ www/src/routes/examples/index.svelte
@@ -0,0 +1,38 @@
+<style>
+  @media only screen and (max-width: 400px) {
+    ul {
+      padding-left: 25px;
+    }
+  }
+  @media only screen and (max-width: 242px) {
+    ul {
+      padding-left: 13px;
+    }
+  }
+</style>
+
+<svelte:head>
+  <title>Svelte-popover — Examples</title>
+  <meta name="keywords" content="Svelte,svelte,svelte.js,sveltejs,features,popover,svelte-popover,examples" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>Examples</h1>
+
+<ul>
+  <li>
+    <a href="/examples/basic">Basic</a>
+  </li>
+  <li>
+    <a href="/examples/eventPropagation">preventDefault && stopPropagation</a>
+  </li>
+  <li>
+    <a href="/examples/hover">Hover</a>
+  </li>
+  <li>
+    <a href="/examples/arrow">Arrow</a>
+  </li>
+  <li>
+    <a href="/examples/onOpenClose">onOpen/onClose</a>
+  </li>
+</ul>
