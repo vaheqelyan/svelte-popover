17665c9306e2b8cb9e9c57438d624b5cd0d71d66
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:41:19 +0400

    Add arrow sample

---
+++ www/src/routes/examples/arrow.svelte
@@ -0,0 +1,66 @@
+<style>
+  .group {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  :global(.popover + .popover) {
+    margin: 0 10px !important;
+  }
+
+  .content {
+    padding: 10px;
+    background: #fff;
+  }
+  .link {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .link a {
+    margin-left: 10px;
+  }
+</style>
+
+<svelte:head>
+  <title>Example — Basic</title>
+  <meta name="description" content="Svelte-grid — Example — Basic" />
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,basic,showcase" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>Arrow</h1>
+
+<a href="examples/onOpenClose">→ onOpen/onClose</a>
+
+<p>
+  <a href="examples/hover">← Hover</a>
+</p>
+
+<p>
+  An arrow is just a symbol ◥. You can change the color of the arrow by setting the
+  <code>arrowColor</code>
+  property, and turn off arrow by setting false for the
+  <code>arrow</code>
+  property
+</p>
+
+<div class="link">
+  <a target="_blank" href={`${openInSource}/basic.svelte`}>View this example's source</a>
+  <a target="_blank" href="">Open in repl</a>
+</div>
+
+<div class="group">
+  <Popover arrowColor="red">
+    <button slot="target">Red arrow</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+
+  <Popover arrowColor="#fff" arrow={false}>
+    <button slot="target">No arrow</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+</div>
+
+<script>
+  import { openInSource } from './github.js';
+  import Popover from 'svelte-popover';
+</script>
