d6d4582fa370048119d32a0a3b49e22693fd5cf6
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:42:20 +0400

    Add hover sample

---
+++ www/src/routes/examples/hover.svelte
@@ -0,0 +1,65 @@
+<style>
+  .group {
+    display: flex;
+    flex-wrap: wrap;
+  }
+  :global(.popover + .popover) {
+    margin: 0 10px !important;
+  }
+
+  .content {
+    padding: 10px;
+    background: #fff;
+  }
+  .link {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .link a {
+    margin-left: 10px;
+  }
+</style>
+
+<svelte:head>
+  <title>Example — Basic</title>
+  <meta name="description" content="Svelte-grid — Example — Basic" />
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,basic,showcase" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>Hover</h1>
+
+<a href="examples/arrow">→ Arrow</a>
+
+<p>
+  <a href="examples/eventPropagation">← preventDefault/stopPropagation</a>
+</p>
+
+<p>Click, hover and touch events</p>
+
+<div class="link">
+  <a target="_blank" href={`${openInSource}/basic.svelte`}>View this example's source</a>
+  <a target="_blank" href="">Open in repl</a>
+</div>
+
+<div class="group">
+  <Popover arrowColor="#fff" action="click">
+    <button slot="target">Click</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+
+  <Popover arrowColor="#fff" action="hover">
+    <button slot="target">Hover</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+
+  <Popover arrowColor="#fff" action={false}>
+    <button slot="target">None</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+</div>
+
+<script>
+  import { openInSource } from './github.js';
+  import Popover from 'svelte-popover';
+</script>
