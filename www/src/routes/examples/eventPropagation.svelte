7dd0b53475ab4baf41b9355f4e2a40372ab72c11
vaheqelyan <martinkenner@mail.com>
2019-09-20 18:41:48 +0400

    Add preventDefault/stopPropagation sample

---
+++ www/src/routes/examples/eventPropagation.svelte
@@ -0,0 +1,53 @@
+<style>
+  .content {
+    padding: 10px;
+    background: #fff;
+  }
+  .link {
+    display: flex;
+    padding-bottom: 20px;
+  }
+  .link a {
+    margin-left: 10px;
+  }
+</style>
+
+<svelte:head>
+  <title>Example — Basic</title>
+  <meta name="description" content="Svelte-grid — Example — Basic" />
+  <meta name="keywords" content="draggable,resizable,grid,layout,responsive,breakpoints,Svelte,svelte,svelte.js,sveltejs,usage,example,examples,samples,basic,showcase" />
+  <meta name="author" content="Vahe Araqelyan" />
+</svelte:head>
+
+<h1>preventDefault/stopPropagation</h1>
+
+<a href="examples/hover">→ Hover</a>
+<p>
+  <a href="examples/basic">← Basic</a>
+</p>
+
+<p>
+  In this example the popover is wrapped in
+  <code>a</code>
+  tag. Pass the
+  <code>preventDefault</code>
+  prop to disable default behavior
+</p>
+
+<div class="link">
+  <a target="_blank" href={`${openInSource}/basic.svelte`}>View this example's source</a>
+  <a target="_blank" href="">Open in repl</a>
+</div>
+
+<a href="https://github.com/vaheqelyan/svelte-grid" target="_blank">
+  Link
+  <Popover arrowColor="#fff" preventDefault>
+    <button slot="target">Popover</button>
+    <div slot="content" class="content">Content</div>
+  </Popover>
+</a>
+
+<script>
+  import { openInSource } from './github.js';
+  import Popover from 'svelte-popover';
+</script>
